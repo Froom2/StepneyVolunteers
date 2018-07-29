ce75500f4d700836018b07a538a5e76750074eea
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2018-07-29 08:30:06 +0100

    initial commit

---
+++ app/controllers/WelcomeController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import javax.inject.Inject
+
+import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+
+class WelcomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def welcome() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.welcome())
+  }
+}
---
+++ test/controllers/WelcomeControllerSpec.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
+
+  "WelcomeController" should {
+    "respond with OK" in {
+
+      val request = FakeRequest(GET, "/welcome")
+      val home = route(app, request).get
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) must include ("Welcome to Stepney Bank Stables!")
+    }
+  }
+
+}
