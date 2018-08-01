21283d62592d015c6c71b2858b9e85fa154b4bcd
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2018-08-01 08:15:13 +0100

    refactor ArrivalController and first test for days

--- app/controllers/arrival/ArrivalController.scala
+++ app/controllers/arrival/ArrivalController.scala
@@ -5,14 +5,18 @@ import javax.inject.Inject
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 import services.TimeService
 
-class ArrivalMonthController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ArrivalController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def arrivalMonth() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.arrival.arrival_month(TimeService.months))
   }
 
-  def selectMonth(month: String) = Action { implicit request: Request[AnyContent] =>
+  def selectArrivalMonth(month: String) = Action { implicit request: Request[AnyContent] =>
     Redirect(controllers.routes.WelcomeController.welcome())
+  }
+
+  def arrivalDay() = Action { implicit request: Request[AnyContent] =>
+    Ok("")
   }
 
 }
--- test/controllers/arrival/ArrivalControllerSpec.scala
+++ test/controllers/arrival/ArrivalControllerSpec.scala
@@ -1,43 +1,42 @@
 package controllers.arrival
 
-import org.scalatest.Matchers
+import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import org.scalacheck.Gen
-import org.scalacheck.Arbitrary.arbitrary
 import services.TimeService
 
-class ArrivalMonthControllerSpec extends PlaySpec with GuiceOneAppPerTest with PropertyChecks {
+class ArrivalControllerSpec extends PlaySpec with GuiceOneAppPerTest with PropertyChecks {
 
-  def controller = new ArrivalMonthController(stubControllerComponents())
+  def controller = new ArrivalController(stubControllerComponents())
 
   "ArrivalMonthController" when {
+
     "arrivalMonth is called" must {
       "respond with OK" in {
-
         val result = controller.arrivalMonth().apply(FakeRequest(GET, "/"))
-
         status(result) mustBe OK
         contentType(result) mustBe Some("text/html")
-
+      }
+    }
+    "selectArrivalMonth is called" must {
+      "respond with SEE_OTHER" in {
+        val monthGenerator = Gen.oneOf(TimeService.months)
+        forAll(monthGenerator) {
+          month =>
+            val result = controller.selectArrivalMonth(month).apply(FakeRequest(GET, "/"))
+            status(result) mustBe SEE_OTHER
+        }
       }
     }
 
-    "selectMonth is called" must {
-      "respond with SEE_OTHER" in {
-
-        val monthGenerator = Gen.oneOf(TimeService.months)
-
-        forAll(monthGenerator) {
-          month =>
-
-            val result = controller.selectMonth(month).apply(FakeRequest(GET, "/"))
-
-            status(result) mustBe SEE_OTHER
-        }
+    "arrivalDay is called" must {
+      "respond with OK" in {
+        val result = controller.arrivalDay().apply(FakeRequest(GET, "/"))
+        status(result) mustBe OK
+        contentType(result) mustBe Some("text/html")
       }
     }
   }
